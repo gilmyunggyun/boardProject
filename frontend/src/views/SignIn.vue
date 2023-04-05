483e8257a27bcc8db45fd72c79a03900fbf2e820
a6791278 <a6791278>
2023-04-05 13:46:05 +0900

    front update

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="container top-0 position-sticky z-index-sticky">
-    <div class="row">
-      <div class="col-12">
-        <navbar
-          is-blur="blur blur-rounded my-3 py-2 start-0 end-0 mx-4 shadow"
-          btn-background="bg-gradient-success"
-          :dark-mode="true"
-        />
-      </div>
-    </div>
-  </div>
+
   <main class="mt-0 main-content main-content-bg">
     <section>
       <div class="page-header min-vh-75">
@@ -19,15 +9,22 @@
               <div class="mt-8 card card-plain">
                 <div class="pb-0 card-header text-start">
                   <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
-                  <p class="mb-0">Enter your email and password to sign in</p>
+                  <p class="mb-0">Enter your id and password to sign in</p>
                 </div>
                 <div class="card-body">
-                  <form role="form" class="text-start">
-                    <label>Email</label>
-                    <vsud-input type="email" placeholder="Email" name="email" />
+                  <form
+                      role="form"
+                      class="text-start"
+                      @submit="checkFrom"
+                      method="get"
+                      >
+                    <label>Id</label>
+                    <vsud-input type="text" placeholder="Id" name="id" isRequired="true"/>
+                    <ul>
+                      <li v-for="error in errors">{{ error }}</li>
+                    </ul>
                     <label>Password</label>
-                    <vsud-input type="password" placeholder="Password" name="password" />
-                    <vsud-switch id="rememberMe" checked>Remember me</vsud-switch>
+                    <vsud-input type="password" placeholder="Password" name="password" isRequired="true" />
                     <div class="text-center">
                       <vsud-button
                         class="my-4 mb-2"
@@ -38,44 +35,29 @@
                     </div>
                   </form>
                 </div>
-                <div class="px-1 pt-0 text-center card-footer px-lg-2">
-                  <p class="mx-auto mb-4 text-sm">
-                    Don't have an account?
-                    <a
-                      href="javascript:;"
-                      class="text-info text-gradient font-weight-bold"
-                    >Sign up</a>
-                  </p>
-                </div>
+
               </div>
             </div>
-            <div class="col-md-6">
-              <div class="top-0 oblique position-absolute h-100 d-md-block d-none me-n8">
-                <div
-                  class="bg-cover oblique-image position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
-                  :style="{
-                    backgroundImage:
-                      `url(${bgImg})`,
-                  }"
-                ></div>
-              </div>
-            </div>
+
           </div>
         </div>
       </div>
     </section>
   </main>
-  <app-footer />
+
 </template>
 
 <script>
+import { ref, reactive } from "vue";
+
 import Navbar from "@/examples/PageLayout/Navbar.vue";
 import AppFooter from "@/examples/PageLayout/Footer.vue";
 import VsudInput from "@/components/VsudInput.vue";
 import VsudSwitch from "@/components/VsudSwitch.vue";
 import VsudButton from "@/components/VsudButton.vue";
-import bgImg from "@/assets/img/curved-images/curved9.jpg"
 const body = document.getElementsByTagName("body")[0];
+
+
 
 export default {
   name: "SigninPage",
@@ -86,10 +68,16 @@ export default {
     VsudSwitch,
     VsudButton,
   },
-  data() {
+  setup() {
+    const errors = ref("");
+
+    const checkFrom = () => {
+      errors.value = "error";
+      return false;
+    };
     return {
-      bgImg
-    }
+      checkFrom, errors
+    };
   },
   beforeMount() {
     this.$store.state.hideConfigButton = true;
